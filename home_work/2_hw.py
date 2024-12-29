8fede682cbca2ed6a8bb2ef8207f21cfa43f64e2
Vladimir <s.v.s.78@yandex.ru>
2024-12-29 15:20:03 +0300

    First Comit

--- home_work/2_hw.py
+++ home_work/2_hw.py
@@ -1,11 +1,16 @@
+
+
 def task_1():
     a: int = 10
     b: float = 52.7
     c: str = "Home Work"
     d: list = [1, 2, 3, 4, 5]
     e: bool = False
-    return a, b, c, d, e
-print(task_1())
+    return type(a), type(b), type(c), type(d), type(e)
+
+result = task_1()
+print(result)
+
 
 def task_2():
     a = [1, 2, 3, 5, 8, 13, 21] #Последовательность Фибоначчи
@@ -13,6 +18,6 @@ def task_2():
 (task_2())
 
 
-def task_3(число: int):
-    return число ** 2
+def task_3(number: int):
+    return number ** 2
 print(task_3(25))
